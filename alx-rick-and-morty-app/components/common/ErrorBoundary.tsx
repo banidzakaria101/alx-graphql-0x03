abe235d6f4593db8ca427e3135efbd92d1e72a53
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-01 17:17:33 +0100

    update

--- alx-rick-and-morty-app/components/common/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/common/ErrorBoundary.tsx
@@ -4,25 +4,25 @@ import { ReactNode } from "react";
 
 interface State {
     hasError: boolean;
-};
+}
 
 interface ErrorBoundaryProps {
     children: ReactNode;
-};
+}
 
 class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
     constructor(props: ErrorBoundaryProps) {
         super(props);
         this.state = { hasError: false};
-    };
+    }
 
     static getDerivedStateFromError(error: Error): State {
         return { hasError: true};
-    };
+    }
 
     componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
         console.log({ error, errorInfo});
-    };
+    }
 
     render() {
         if(this.state.hasError) {
@@ -36,8 +36,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
             );
         }
         return this.props.children;
-    };
-};
-
+    }
+}
 
 export default ErrorBoundary;
